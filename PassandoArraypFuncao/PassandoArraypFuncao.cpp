df57d0265c103951dac6a48d4435818746131ac8
Joao Cursino <fabrica.render@gmail.com>
2023-08-10 15:47:42 -0300

    PassandoArraypFuncao-Pt3

--- PassandoArraypFuncao/PassandoArraypFuncao.cpp
+++ PassandoArraypFuncao/PassandoArraypFuncao.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <array>
+
 
 void MostrarVetor(int Array[], int TamVetor);
 
@@ -8,9 +10,9 @@ int main()
 	int Vetor[] = { 1,2,3,4,5,6,7,8,9,10 };
 
 	//Sizeof devolve o numero em bytes do tamanho de uma variavel,logo sizeof(Vetor) vai devolver o numero de elementos do vetor multiplicado pelo tamannho do seu tipo
-	int TamanhoVetor = sizeof(Vetor);
+	int TamanhoVetor = std::size(Vetor);
 
-
+	// Apartir de VS 2017 voce ´pode usar std::size (sem informar o tipo de variavel)
 	std::cout << "\nTamanho Vetor: " << TamanhoVetor;
 	std::cout << "\nTamanho de um float: " << sizeof(float);
 	std::cout << "\nTamanho de um int: " << sizeof(int);
